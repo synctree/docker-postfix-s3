ffd861e5c4a3f8d51fd9c479319239f69565c9ba
Bryan Conrad <bkconrad@gmail.com>
2015-12-18 09:51:53 -0800

    add printf-like support for whitelist destinations

--- README.md
+++ README.md
@@ -7,6 +7,16 @@ docker-postfix
 
 + `MAIL_DOMAIN`: The domain to accept mail on (e.g. `inbox.example.org`)
 + `S3_BUCKET`: The bucket name to send mail to (e.g. `my-mail-bucket`)
++ `WHITELIST_URI`: The full URI to the whitelist file (e.g. `my-bucket/my-whitelist.yml`)
+
+## Whitelist Format
+
+```
+---
+senderaddress@theirdomain.com: prefix/%s/%n
+```
+
+`%t` is replaced with the unix timestamp when the file is received, `%n` is replaced with the incoming file name (cleaned by `munpack`)
 
 ## Instance Role
 ```
--- assets/filter.sh
+++ assets/filter.sh
@@ -9,13 +9,14 @@ cd $WORKDIR
 
 # parse destination directory
 aws --region us-east-1 s3 cp "s3://$WHITELIST_URI" "/tmp/postfix-s3-whitelist" >> /tmp/filter.log 2>&1
-directory="$(grep "$SENDER" /tmp/postfix-s3-whitelist | head -n1 | cut -d: -f2 | sed -e 's/ *//')"
+timestamp="$(date +%s)"
 
 # unpacks the attachments read from stdin
 munpack | while read line ; do
   filename="$(echo "$line" | cut -d\  -f1)"
+  destination="$(grep "$SENDER" /tmp/postfix-s3-whitelist | head -n1 | cut -d: -f2 | sed -e 's/ *//' | sed -e "s/%t/$timestamp/g" | sed -e "s/%n/$filename/g")"
   echo "Received mail with $@, attached file $filename" >> "$LOGFILE"
-  aws --region us-east-1 s3 cp "$filename" "s3://$S3_BUCKET/$directory/$(date +%s)-$filename" >> /tmp/filter.log 2>&1
+  aws --region us-east-1 s3 cp "$filename" "s3://$S3_BUCKET/$destination" >> /tmp/filter.log 2>&1
 done
 
 # clean up
