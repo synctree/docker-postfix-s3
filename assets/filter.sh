4c33cd51144721c69bff318d4fe32d3ab3a665af
Bryan Conrad <bkconrad@gmail.com>
2015-12-18 07:41:36 -0800

    add whitelist uri permissions to instance role

--- assets/filter.sh
+++ assets/filter.sh
@@ -8,7 +8,7 @@ WORKDIR=`mktemp -d /tmp/filter-$(date +%s)-XXXX`
 cd $WORKDIR
 
 # parse destination directory
-aws --region us-east-1 s3 cp "$WHITELIST_URI" "/tmp/postfix-s3-whitelist" >> /tmp/filter.log 2>&1
+aws --region us-east-1 s3 cp "s3://$WHITELIST_URI" "/tmp/postfix-s3-whitelist" >> /tmp/filter.log 2>&1
 directory="$(grep "$SENDER" /tmp/postfix-s3-whitelist | head -n1 | cut -d: -f2 | sed -e 's/ *//')"
 
 # unpacks the attachments read from stdin
