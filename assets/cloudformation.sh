a6a4b6094c7e74409023cf9467d953bc134b4700
Bryan Conrad <bkconrad@gmail.com>
2015-12-18 06:59:56 -0800

    export path in CF script

--- assets/cloudformation.sh
+++ assets/cloudformation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 /bin/bash /opt/docker-postfix-s3/assets/build.sh
 /bin/bash /opt/docker-postfix-s3/assets/install.sh
 sed -e 's/nodaemon=true/nodaemon=false/' -i /etc/supervisor/conf.d/supervisord.conf
